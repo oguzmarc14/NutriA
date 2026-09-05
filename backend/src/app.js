650e2522306b72eb7d90d5fb4c189ad50b36e13d
oguzmarc14 <oguzmarc14@gmail.com>
2026-09-04 18:47:14 -0600

    feat: conecta rutas de pacientes a la API

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 const cors = require('cors')
 const express = require('express')
 const authRoutes = require('./routes/auth.routes')
+const pacientesRoutes = require('./routes/pacientes.routes')
 
 const app = express()
 
